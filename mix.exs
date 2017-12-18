5a22b57ebc86d089d8857439f9a14a1fa33740cb
Guanting Chen (@Sif) <cgt886@gmail.com>
2017-12-19 02:03:27 +0800

    update mix config ( 1.0.0 )

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RemoveEmoji.Mixfile do
     [
       app: :remove_emoji,
       name: "RemoveEmoji",
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       description: description(),
@@ -24,8 +24,6 @@ defmodule RemoveEmoji.Mixfile do
   defp deps do
     [
       {:ex_doc, "~> 0.14", only: :dev},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 
@@ -35,9 +33,7 @@ defmodule RemoveEmoji.Mixfile do
 
   defp package do
     [
-      # This option is only needed when you don't want to use the OTP application name
       name: "remove_emoji",
-      # These are the default files included in the package
       files: ["lib", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Guanting Chen"],
       licenses: ["MIT"],
